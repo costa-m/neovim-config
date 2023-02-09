1797540143c01d2e911da9974a4d455fba1b2e42
costa-m <costa.m@fastmail.com>
2023-02-08 22:08:49 -0300

    remaps

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -12,17 +12,33 @@ vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
 -- greatest remap ever
+-- pasting foo into bar retains foo in the vim clipboard
+-- bar gets sent to void register
 vim.keymap.set("x", "<leader>p", [["_dP]])
 
 -- next greatest remap ever : asbjornHaland
+-- copy to system clipboard
 vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 
+-- more deleting to void register
 vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
 
+-- doing this because <C-C> and <C-[> don't work with column mode edit
+vim.keymap.set("i", "<C-[>", "<Esc>")
 vim.keymap.set("i", "<C-c>", "<Esc>")
 
+-- this replays last recorded macro
 vim.keymap.set("n", "Q", "<nop>")
 
 -- saving
 vim.keymap.set("n", "<leader>w", ":update<CR>")
+
+-- for vim quickfix mode
+vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
+vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
+vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
+vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
+
+-- replacement for current word
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
