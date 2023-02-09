fabb31bb5bf726a0dfab8e974e10af1f0cec8865
costa-m <costa.m@fastmail.com>
2023-02-08 22:04:05 -0300

    disable treesitter for telescope preview for some languages

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -4,3 +4,16 @@ vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 vim.keymap.set('n', '<leader>ps', function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") })
 end)
+
+-- It's possible that telescope was freezing during fuzzy search,
+-- because of treesitter. This configuration disables it for the
+-- languages specified.
+-- https://github.com/nvim-telescope/telescope.nvim/pull/1612
+require('telescope').setup({
+    defaults = {
+        preview = {
+            treesitter = false,
+            languages = { "javascript", "php", "html" }
+        },
+    },
+})
