3237dfa769b7848fe8995feb7e5a869fe906900c
costa-m <costa.m@fastmail.com>
2023-12-08 01:00:45 -0300

    add commands for text and file finder that disregard gitignore

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,16 +1,34 @@
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+vim.keymap.set('n', '<leader>pfi', builtin.find_files, {})
+vim.keymap.set('n', '<leader>pfa', function()
+    builtin.find_files({ no_ignore = true, hidden = true })
+end)
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-vim.keymap.set('n', '<leader>ps', function()
-	builtin.grep_string({ search = vim.fn.input("Grep > ") })
+vim.keymap.set('n', '<leader>psi', function()
+    builtin.grep_string({ search = vim.fn.input("Grep > ") })
+end)
+vim.keymap.set('n', '<leader>psa', function()
+    builtin.grep_string({
+        search = vim.fn.input("Grep > "),
+        vimgrep_arguments = {
+            'rg',
+            '--color=never',
+            '--no-heading',
+            '--with-filename',
+            '--line-number',
+            '--column',
+            '--smart-case',
+            '-u' -- disregard gitignore
+        },
+    })
 end)
 
--- It's possible that telescope was freezing during fuzzy search,
--- because of treesitter. This configuration disables it for the
--- languages specified.
--- https://github.com/nvim-telescope/telescope.nvim/pull/1612
 require('telescope').setup({
     defaults = {
+        -- It's possible that telescope was freezing during fuzzy search,
+        -- because of treesitter. This configuration disables it for the
+        -- languages specified.
+        -- https://github.com/nvim-telescope/telescope.nvim/pull/1612
         preview = {
             treesitter = false,
             languages = { "javascript", "php", "html" }
